a43cc9aa46cb12ea348682380576cf0bbe64c2e6
VedKhairnar24 <vedkhairnar4@gmail.com>
2025-08-27 20:57:43 +0530

    create a lecture 3 file and learn about for loop and while loop

---
+++ lecture3.js
@@ -0,0 +1,14 @@
+// loop's
+/*
+for Loop's..........................
+for(initialisation; Condition; Updation)
+
+*/
+// let times;
+// for (let i = 0; i <= 10; i++){
+//     console.log(i);
+//     times = i++;
+// }
+
+// console.log("loops",times, "run");
+
